6e57ef37260e894cec5d108afd864553dd595b50
sneharajpurkar <snehasrajpurkar@gmail.com>
2023-06-14 18:28:08 +0530

    Updated

--- projectp/src/App.jsx
+++ projectp/src/App.jsx
@@ -1,13 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
 import Home from './components/Home';
 
 function App() {
   return (
     <>
-    <Navbar/>
     <Routes>
       <Route exact path='/' element={<Home/>}/>
     </Routes>
